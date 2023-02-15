64403ef26955d423152b18186742cd9bb422c10b
Stefan Pofahl <stefan@pofahl.de>
2023-02-15 14:26:21 +0100

    twin axis / 2nd y-axis added

--- GLMakie/changing_attributes_in_existing_screen_objects.jl
+++ GLMakie/changing_attributes_in_existing_screen_objects.jl
@@ -7,17 +7,25 @@
 # ... ......................................................................................................................... #
 # --- change color inside text: rich():                                                                                         #
 # --- https://docs.makie.org/stable/examples/plotting_functions/text/index.html#rich_text                                       #
-# --- control axis size, info at the bottom of:                                                                                 #
+# --- second y-axis / twin axis: ---------------------------------------------------------------------------------------------- #
+# --- https://docs.makie.org/stable/examples/blocks/axis/index.html#creating_a_twin_axis                                        #
+# --- control axis size, see info at the bottom of: --------------------------------------------------------------------------- #
 # --- https://docs.makie.org/stable/tutorials/aspect-tutorial/index.html                                                        #
 # ... ......................................................................................................................... #
 using GLMakie
 # fig = Figure(resolution = (400, 600))
 fig = Figure()
 
-bt = Button(fig[1, 1]; label = rich(rich("on", color = :red), "/", rich("off", color = :black)), strokewidth = 4, strokecolor = (:green), tellheight = true, )
-ax = Axis(fig[2, 1]; title = "test", aspect = DataAspect(), width = 150, height = 150, tellheight = true, tellwidth = true)
-l1 = lines!(ax, [1,2,3])
-l2 = lines!([3,2,1])
+bt = Button(fig[1, 1]; label = rich(rich("on", color = :red), "/", rich("off", color = :black)), 
+        strokewidth = 4, strokecolor = (:green), tellheight = true, )    
+ax1 = Axis(fig[2, 1]; title = "test", titlegap = 30.0, aspect = DataAspect(), width = 150, height = 150, 
+        yticklabelcolor = :blue, tellheight = true, tellwidth = true)
+# --- 2nd y-axis / twin axis:        
+ax2 = Axis(fig[2, 1]; yticklabelcolor = :green, xaxisposition = :top, yaxisposition = :right)
+# ---
+l1 = lines!(ax1, [1,2,3])
+l1_clr = l1.color
+l2 = lines!(ax2, [6,4,2])
 # ---
 on(bt.clicks) do nclicks 
     l2.visible = false # trick if change of object attribute does not change, switch on/off visibility might help
@@ -37,4 +45,4 @@ end
 # ---
 resize_to_layout!(fig)
 scene_obj = display(fig)
-wait(scene_obj)
+# wait(scene_obj)
