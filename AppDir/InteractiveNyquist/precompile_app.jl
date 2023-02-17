1e5c203d2718d37b6a4a64c91cf31720701b6329
WC1991 <s51079@edu.campus02.at>
2023-02-17 23:59:34 +0100

    I d not know, if a "precompile_app.jl" makes sense

--- AppDir/InteractiveNyquist/precompile_app.jl
+++ AppDir/InteractiveNyquist/precompile_app.jl
@@ -1,17 +1,18 @@
-import Tulip, GLMakie, Pkg
-if isfile("Project.toml") 
-    Pkg.activate(".")
-else
-    cd("InteractiveNyquist")
-    if isfile("Project.toml") 
-        Pkg.activate(".")
-    else
-        error("----------------  Project.toml is missing!   ------------------------")
-    end
-end
-Pkg.resolve()
-Pkg.build("Tulip")
-Pkg.build("GLMakie")
-Tulip.__init__();
-GLMakie.Makie.__init__();
-@info("======  End Precompile_app.jl   ======")
+# I do not know what to do here ... :-(
+# import Tulip, GLMakie, Pkg
+# if isfile("Project.toml") 
+#     Pkg.activate(".")
+# else
+#     cd("InteractiveNyquist")
+#     if isfile("Project.toml") 
+#         Pkg.activate(".")
+#     else
+#         error("----------------  Project.toml is missing!   ------------------------")
+#     end
+# end
+# Pkg.resolve()
+# Pkg.build("Tulip")
+# Pkg.build("GLMakie")
+# Tulip.__init__();
+# GLMakie.Makie.__init__();
+# @info("======  End Precompile_app.jl   ======")
